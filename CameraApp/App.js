923eb5b78aa909aed17ea3dc4489cff8f9fed7a4
Kumario1 <pkumar13.ipec@gmail.com>
2024-12-20 10:31:16 -0600

    Finished adding the camera functionality. Next step -> add the functionality to take a picture and then save it to camera role.
    after that try to add pre built filters, etc, etc.
    
    once that is complete. I will add the main functionality of the app. Using maybe AI or a camera engine I will convert the Image to have a digital camera effect. I want the camera to process the image for a long time, and make it have the same effect a digital camera will.

--- CameraApp/App.js
+++ CameraApp/App.js
@@ -1,10 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { Camera, CameraType }  from 'expo-camera';
-import * as MediaLibrary from 'expo-media-library'
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { CameraView, CameraType, useCameraPermissions }  from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+import React, {useState, useEffect, useRef} from 'react';
 
 export default function App() {
+  //getting permission, using a react hook to change the persmissions
+  const [permission, requestPermission] = useCameraPermissions();
+  //setting up the image to display, and using react hook to change image
+  const [image, setImage] = useState(null);
+  const [facing, setFacing] = useState('back');
+  const cameraRed = useRef(null);
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
   return (
     <View style={styles.container}>
+      <CameraView style={styles.camera} facing={facing}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
     </View>
   );
 }
@@ -12,8 +46,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
