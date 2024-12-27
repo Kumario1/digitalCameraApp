338152da3780efd99755da20e67375f42d4ef7de
visshbala21 <visshbala12@gmail.com>
2024-12-26 22:16:55 -0600

    changes.

--- flask-server/server.py
+++ flask-server/server.py
@@ -470,4 +470,5 @@ def upload_and_filter():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
+
